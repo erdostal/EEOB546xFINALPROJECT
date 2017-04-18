7e433430d7332b4decdfb053256fb470fb5c14b9
Corrinne Grover <corrinne@iastate.edu>
2017-04-18 15:59:33 -0500

    Update top.clusters.R
    added in the Fig 4 code, renamed the glm stuff we were working on to proper figure number

--- top.clusters.R
+++ top.clusters.R
@@ -64,7 +64,50 @@ Species <- as.factor(melted$Var1)
 Cluster <- as.factor(melted$Var2)
 anova(lm(Y~Cluster*Species))
 
-#Recreate Figure 4 #still needs work 
+#Recreate Figure 3 #still needs work 
 new_table <- as.data.frame(t(new_table))
 linear_model <- glm(A1~A2, data=new_table)
 ggplot(new_table, aes(x=A1, y=A2)) + geom_point(size=2) + geom_abline(intercept=0, slope=1)
+
+#Recreate Figure 4, not pretty
+
+# import file from comparative analysis table and take top 1000 clusters, as per SRB paper
+clust <- read.table("table_clusters.txt", header = T, sep="\t")
+clust <- head(clust, 1000)
+
+#ordination analysis
+
+#transform data and make the euclidean distance matrix
+mydata <- t(clust)
+d <- dist(mydata, method = "euclidean")
+
+#SRB performs classical MDS (principal coordinate analysis) using cmdscale, minimally
+#he mentions this package specifically, which I why I don't know where vegan comes in
+cmdfit <- cmdscale(d,eig=TRUE, k=2) # k is the number of dim
+x <- cmdfit$points[,1]
+y <- cmdfit$points[,2]
+
+png("SRB.Fig4.png", 5000, 5000, pointsize=12, res=600)
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", main="Metric MDS",	type="n")
+text(x, y, labels = row.names(mydata), cex=.7)
+points(x, y, pch=19)
+dev.off()
+
+# Simon also claimed to use vegan/ordihull/ordispider to do this analysis
+# ordihull/spider appear to rely on vegan output
+# there are not enough details to determine if he used vegan for the ordination analysis
+# and how he would have conducted it: community dissimilarities, eigenvector methods, detrended correspondence analysis, etc
+# not to mention there are tons of other options
+# AFAIK ordihull/spider appears to make the output of vegan more pretty
+
+
+
+
+
+
+
+
+
+
+
+
